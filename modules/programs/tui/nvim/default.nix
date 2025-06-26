ca5b4caf06fdd1fb43825bdfa0cc4c0eafa3bb25
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 15:53:57 +0900

    Add todo

--- modules/programs/tui/nvim/default.nix
+++ modules/programs/tui/nvim/default.nix
@@ -7,8 +7,10 @@
   cfg = config.custom.programs.nvim;
 in {
   options.custom.programs.nvim.enable = lib.mkEnableOption {};
+  # TODO: Port nvim config to Nixvim (or nvf)
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
+      nvim
       markdownlint-cli2
     ];
   };

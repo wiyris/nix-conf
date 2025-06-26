71259d235b9aa60bb1706d1acc90656cbf531e06
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 19:05:30 +0900

    change default terminal-emulator

--- modules/desktop/window-managers/hyprland/dots/default_apps.nix
+++ modules/desktop/window-managers/hyprland/dots/default_apps.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     "$mainMod" = "SUPER";
-    "$terminal" = "kitty";
+    "$terminal" = "ghostty";
     "$launcher" = "rofi -show drun";
     "$browser" = "zen-beta";
     "$bar" = "waybar";

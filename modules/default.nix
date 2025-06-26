6af979b8191c590d08babd9b5cfa025b01a7e3d8
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 20:34:02 +0900

    Organize file locations

--- modules/default.nix
+++ modules/default.nix
@@ -35,42 +35,40 @@
 
     ./programs/cli/aliases
     ./programs/cli/bat
+    ./programs/cli/btop
+    ./programs/cli/cava
     ./programs/cli/core-utils
     ./programs/cli/eza
     ./programs/cli/fastfetch
     ./programs/cli/fish
     ./programs/cli/fzf
     ./programs/cli/git
+    ./programs/cli/gtrash
+    ./programs/cli/htop
+    ./programs/cli/lazygit
+    ./programs/cli/nvim
     ./programs/cli/ripgrep
     ./programs/cli/starship
+    ./programs/cli/tmux
+    ./programs/cli/yazi
     ./programs/cli/zoxide
 
-    ./programs/tui/btop
-    ./programs/tui/cava
-    ./programs/tui/gtrash
-    ./programs/tui/htop
-    ./programs/tui/lazygit
-    ./programs/tui/nvim
-    ./programs/tui/tmux
-    ./programs/tui/yazi
-
-    ./programs/gui/chromium
-    ./programs/gui/electron
-    ./programs/gui/foot
-    ./programs/gui/ghostty
-    ./programs/gui/goldendict
-    ./programs/gui/gtk
-    ./programs/gui/kitty
-    ./programs/gui/mpv
-    ./programs/gui/pqiv
-    ./programs/gui/qt
-    ./programs/gui/spotify
-    ./programs/gui/thunar
-    ./programs/gui/zen
-
-    ./programs/gaming/osu
-    ./programs/gaming/optimize
-    ./programs/gaming/steam
+    ./programs/gui/browser/chromium
+    ./programs/gui/browver/zen
+    ./programs/gui/gaming/optimize
+    ./programs/gui/gaming/osu
+    ./programs/gui/gaming/steam
+    ./programs/gui/general/electron
+    ./programs/gui/general/goldendict
+    ./programs/gui/general/gtk
+    ./programs/gui/general/qt
+    ./programs/gui/general/thunar
+    ./programs/gui/media/mpv
+    ./programs/gui/media/pqiv
+    ./programs/gui/media/spotify
+    ./programs/gui/terminal/foot
+    ./programs/gui/terminal/ghostty
+    ./programs/gui/terminal/kitty
 
     ./services/batsignal
     ./services/keyd

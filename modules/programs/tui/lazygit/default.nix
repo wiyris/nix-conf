3af1cc333f6c9b3cec409a4d1b0b4458b1777ebf
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 13:35:18 +0900

    add lazygit module

--- modules/programs/tui/lazygit/default.nix
+++ modules/programs/tui/lazygit/default.nix
@@ -5,7 +5,7 @@
 }: let
   cfg = config.custom.programs.lazygit;
 in {
-  options.custom.programs.lagygit.enable = lib.mkEnableOption {};
+  options.custom.programs.lazygit.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm.programs.lazygit = {
       enable = true;

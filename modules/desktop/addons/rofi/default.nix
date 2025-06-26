f3a29a91f08322f98dee166fda27f0d0fedfa2c9
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 19:44:52 +0900

    add powermenu script

--- modules/desktop/addons/rofi/default.nix
+++ modules/desktop/addons/rofi/default.nix
@@ -5,9 +5,9 @@
   ...
 }: let
   cfg = config.desktop.rofi;
-  # powermenu =
-  #   pkgs.writeScriptBin "tofi-powermenu"
-  #   (builtins.readFile ./scripts/tofi-powermenu.sh);
+  powermenu =
+    pkgs.writeScriptBin "powermenu"
+    (builtins.readFile ./scripts/powermenu.sh);
   # screenshot =
   #   pkgs.writeScriptBin "tofi-emoji"
   #   (builtins.readFile ./scripts/tofi-emoji.sh);
@@ -21,12 +21,14 @@ in {
       home.packages = [
         pkgs.rofi-wayland
 
+        powermenu
         wallpaper
       ];
       xdg.configFile = {
         "rofi/config.rasi".source = ./dots/config.rasi;
         "rofi/style.rasi".source = ./dots/style.rasi;
         "rofi/theme/catppuccin-mocha.rasi".source = ./dots/theme/catppuccin-mocha.rasi;
+        "rofi/theme/powermunu.rasi".source = ./dots/powermenu.rasi.rasi;
         "rofi/theme/wallpaper.rasi".source = ./dots/wallpaper.rasi;
       };
     };

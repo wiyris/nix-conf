6fff96eb498eef039c5371beef8f5fa2c61d5971
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 15:31:39 +0900

    Add conditional check

--- modules/desktop/addons/swayosd/default.nix
+++ modules/desktop/addons/swayosd/default.nix
@@ -13,14 +13,14 @@ in {
       };
 
       # override hyprland binds
-      wayland.windowManager.hyprland.settings.bindel = lib.mkForce [
+      wayland.windowManager.hyprland.settings.bindel = lib.mkIf config.desktop.hyprland.enable (lib.mkForce [
         ", XF86AudioRaiseVolume,  exec, swayosd-client --output-volume +5"
         ", XF86AudioLowerVolume,  exec, swayosd-client --output-volume -5"
         ", XF86AudioMute,         exec, swayosd-client --output-volume mute-toggle"
         ", XF86AudioMicMute,      exec, swayosd-client --input-volume mute-toggle"
         ", XF86MonBrightnessUp,   exec, swayosd-client --brightness +10"
         ", XF86MonBrightnessDown, exec, swayosd-client --brightness -10"
-      ];
+      ]);
     };
   };
 }

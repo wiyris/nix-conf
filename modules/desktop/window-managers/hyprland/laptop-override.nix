b4910ad82f77217b0785b1a4da5bbb79ee3a1824
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-26 12:53:49 +0900

    Relocate laptop-override

--- modules/desktop/window-managers/hyprland/laptop-override.nix
+++ modules/desktop/window-managers/hyprland/laptop-override.nix
@@ -5,15 +5,15 @@
     decoration = {
       blur.enabled = lib.mkForce false;
       shadow.enabled = lib.mkForce false;
-      rounding = lib.mkForce 0;
+      rounding = lib.mkForce 4;
       # active_opacity = lib.mkForce 1.0;
       # inactive_opacity = lib.mkForce 1.0;
       # fullscreen_opacity = lib.mkForce 1.0;
       # dim_inactive = lib.mkForce true;
     };
     general = {
-      gaps_in = lib.mkForce 0;
-      gaps_out = lib.mkForce 0;
+      gaps_in = lib.mkForce 2;
+      gaps_out = lib.mkForce 6;
     };
   };
 }

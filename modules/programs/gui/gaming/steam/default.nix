5c628326896631ccb1e91067db632d78fb6e2b4e
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-27 15:05:17 +0900

    Enable gamescope

--- modules/programs/gui/gaming/steam/default.nix
+++ modules/programs/gui/gaming/steam/default.nix
@@ -10,7 +10,7 @@ in {
   config = lib.mkIf cfg.enable {
     programs.steam = {
       enable = true;
-      # gamescopeSession.enable = true;
+      gamescopeSession.enable = true;
       extraCompatPackages = with pkgs; [proton-ge-bin];
       # remotePlay.openFirewall = true;
       # dedicatedServer.openFirewall = true;

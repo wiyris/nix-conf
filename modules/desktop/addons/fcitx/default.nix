09594ca9c34e9398b63c58257c3f63d223dc09ea
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-06-27 15:04:54 +0900

    remove gtk fcitx frontend env

--- modules/desktop/addons/fcitx/default.nix
+++ modules/desktop/addons/fcitx/default.nix
@@ -24,7 +24,7 @@ in {
 
     hm.home.sessionVariables = {
       QT_IM_MODULE = "fcitx";
-      GTK_IM_MODULE = "fcitx";
+      # GTK_IM_MODULE = "fcitx";
       XMODIFIERS = "@im=fcitx";
       SDL_IM_MODULE = "fcitx";
       GLFW_IM_MODULE = "ibus";

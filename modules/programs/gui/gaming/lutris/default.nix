e4e4e6f336b2a5d7db02c006f94edb274a08a67d
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-05 15:21:51 +0900

    Fix typo

--- modules/programs/gui/gaming/lutris/default.nix
+++ modules/programs/gui/gaming/lutris/default.nix
@@ -5,7 +5,7 @@
 }: let
   cfg = config.gaming.lutris;
 in {
-  options.gaming.luris.enable = lib.mkEnableOption {};
+  options.gaming.lutris.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm.programs.lutris = {
       enable = true;

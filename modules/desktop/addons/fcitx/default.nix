4b8f0fffdda0e603136a77f8283d49e54beb9f98
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-06 01:19:34 +0900

    Mask systemd fcitx autostart

--- modules/desktop/addons/fcitx/default.nix
+++ modules/desktop/addons/fcitx/default.nix
@@ -11,6 +11,7 @@
 in {
   options.desktop.fcitx.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
+    systemd.user.services."app-org.fcitx.Fcitx5@autostart".enable = false;
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";

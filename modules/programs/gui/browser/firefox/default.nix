4fb651e98b079511fa0c71945e0f0b5a94197825
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-06 19:38:02 +0900

    wip:add user profile

--- modules/programs/gui/browser/firefox/default.nix
+++ modules/programs/gui/browser/firefox/default.nix
@@ -1,17 +1,39 @@
 {
-  inputs,
   lib,
-  pkgs,
   config,
   ...
 }: let
   cfg = config.custom.programs.firefox;
+  inherit (config.globals) userName;
 in {
   options.custom.programs.firefox.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm = {
+      stylix.targets.firefox.profileNames = ["${userName}"];
       programs.firefox = {
         enable = true;
+        policies = {
+          DisableTelemetry = true;
+          DisablePocket = true;
+          DisableFirefoxStudies = true;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          OverrideFirstRunPage = "";
+          OverridePostUpdatePage = "";
+          ExtensionSettings = {
+            # Disable built-in search engines
+            "amazondotcom@search.mozilla.org" = {installation_mode = "blocked";};
+            "bing@search.mozilla.org" = {installation_mode = "blocked";};
+            "ddg@search.mozilla.org" = {installation_mode = "blocked";};
+            "ebay@search.mozilla.org" = {installation_mode = "blocked";};
+          };
+          profiles."&{usarName}" = {
+          };
+        };
       };
     };
   };

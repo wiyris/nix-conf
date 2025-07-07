f6ab83c26a1ef7edab369392213d955f6f47b35d
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-07 23:48:49 +0900

    add extension

--- modules/programs/gui/browser/firefox/default.nix
+++ modules/programs/gui/browser/firefox/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   inputs,
   config,
   ...
@@ -12,18 +13,26 @@ in {
   config = lib.mkIf cfg.enable {
     hm = {
       stylix.targets.firefox.profileNames = ["${userName}"];
+      # imports = [inputs.nur.modules.nixos.default];
       programs.firefox = {
         enable = true;
         policies = import ./dots/policies.nix;
         profiles.${userName} = {
           search = import ./dots/search.nix;
+          extensions.packages = with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons; [
+            kagi-search
+            proton-pass
+            simplelogin
+            stylus
+            surfingkeys
+            ublock-origin
+          ];
 
           extraConfig = ''
             ${builtins.readFile "${betterfox}/user.js"}
             ${builtins.readFile "${betterfox}/Fastfox.js"}
             ${builtins.readFile "${betterfox}/Peskyfox.js"}
             ${builtins.readFile "${betterfox}/Smoothfox.js"}
-            ${builtins.readFile "./dots/override.js"}
           '';
         };
       };

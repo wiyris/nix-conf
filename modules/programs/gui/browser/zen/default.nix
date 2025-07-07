355e39cf179c1b6d10dc91bb9b2da2b89b41f8d8
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-08 00:29:54 +0900

    update

--- modules/programs/gui/browser/zen/default.nix
+++ modules/programs/gui/browser/zen/default.nix
@@ -5,16 +5,28 @@
   config,
   ...
 }: let
+  betterfox = inputs.betterfox;
   cfg = config.custom.programs.zen;
 in {
   options.custom.programs.zen.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [inputs.zen-browser.packages."${pkgs.system}".default];
+    hm = {
+      imports = [inputs.zen-browser.homeModules.beta];
+      programs.zen-browser = {
+        enable = true;
+        # extraConfig = ''
+        #   ${builtins.readFile "${betterfox}/zen/user.js"}
+        # '';
+        home.File = {
+        };
+      };
 
-    hm.xdg.mimeApps.defaultApplications = {
-      "text/html" = "zen-beta.desktop";
-      "x-scheme-handler/http" = "zen-beta.desktop";
-      "x-scheme-handler/https" = "zen-beta.desktop";
+      xdg.mimeApps.defaultApplications = {
+        "text/html" = "zen-beta.desktop";
+        "x-scheme-handler/http" = "zen-beta.desktop";
+        "x-scheme-handler/https" = "zen-beta.desktop";
+      };
     };
   };
 }

516c89efd6d88d0503fb702e64ef757f98105b23
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-07 23:10:00 +0900

    test:add betterfox's user.js

--- modules/programs/gui/browser/firefox/default.nix
+++ modules/programs/gui/browser/firefox/default.nix
@@ -5,19 +5,13 @@
   config,
   ...
 }: let
-  betterfox = pkgs.fetchFromGitHub {
-    owner = "yokoffing";
-    repo = "Betterfox";
-    rev = "09dd87a3abcb15a88798941e5ed74e4aa593108c";
-    hash = "sha256-Uu/a5t74GGvMIJP5tptqbiFiA+x2hw98irPdl8ynGoE=";
-  };
+  betterfox = inputs.betterfox;
   cfg = config.custom.programs.firefox;
   inherit (config.globals) userName;
 in {
   options.custom.programs.firefox.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm = {
-      imports = [inputs.arkenfox-nixos.hmModules.arkenfox];
       stylix.targets.firefox.profileNames = ["${userName}"];
       programs.firefox = {
         enable = true;

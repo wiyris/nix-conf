6f0a56e56af78dd0fada85f3ecbf451533f6fe1e
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-08 00:15:08 +0900

    add leta brave

--- modules/programs/gui/browser/firefox/dots/search.nix
+++ modules/programs/gui/browser/firefox/dots/search.nix
@@ -2,15 +2,23 @@
   default = "leta";
   force = true;
   engines = {
-    leta = {
-      name = "leta";
+    leta.google = {
+      name = "leta.google";
       urls = [{template = "https://leta.mullvad.net/search?q={searchTerms}&engine=google";}];
-      definedAliases = ["@l"];
+      definedAliases = ["@lg"];
     };
+
+    leta.brave = {
+      name = "leta.brave";
+      urls = [{template = "https://leta.mullvad.net/search?q={searchTerms}&engine=brave";}];
+      definedAliases = ["@lb"];
+    };
+
+    "bing".metaData.hidden = true;
     "ddg".metaData.hidden = true;
+    "ecosia".metaData.hidden = true;
     "google".metaData.hidden = true;
     "qwant".metaData.hidden = true;
-    "bing".metaData.hidden = true;
-    "ecosia".metaData.hidden = true;
+    "wikipedia".metaData.hidden = true;
   };
 }

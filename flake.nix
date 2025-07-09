7b52218575a3889db77a5e05a38d3cf93a2c5dfc
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-09 17:29:07 +0900

    Add stable channel hm

--- flake.nix
+++ flake.nix
@@ -40,6 +40,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";

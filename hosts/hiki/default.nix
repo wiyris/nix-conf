9329162a4e8f93b7b771bed2466a2b107daff4b2
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-09 19:57:38 +0900

    tweak

--- hosts/hiki/default.nix
+++ hosts/hiki/default.nix
@@ -22,12 +22,27 @@ in {
     # cli
     programs.fish.enable = true;
 
+    custom.programs.aliases.enable = true;
+    custom.programs.bat.enable = true;
     custom.programs.core-utils.enable = true;
+    custom.programs.eza.enable = true;
     custom.programs.fish.enable = true;
+    custom.programs.fzf.enable = true;
+    custom.programs.git.enable = true;
+    custom.programs.ripgrep.enable = true;
+    custom.programs.starship.enable = true;
+    custom.programs.zoxide.enable = true;
+
+    # tui
+    custom.programs.btop.enable = true;
+    custom.programs.gtrash.enable = true;
+    custom.programs.htop.enable = true;
+    custom.programs.tmux.enable = true;
+    custom.programs.yazi.enable = true;
 
     time.timeZone = "Asia/Tokyo";
 
-    # boot.loader.timeout = 0;
+    boot.loader.timeout = 0;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     system.stateVersion = "25.05"; # Did you read the comment?

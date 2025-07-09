a98eee94304ad8270f4f8d5275ad7c0e6f0b98f6
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-09 09:38:38 +0900

    add qemu

--- modules/programs/cli/core-utils/default.nix
+++ modules/programs/cli/core-utils/default.nix
@@ -9,31 +9,34 @@ in {
   options.custom.programs.core-utils.enable = lib.mkEnableOption {};
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      git
-      vim
-      neovim
-      curl
-      wget
-      file # print filetype
-      jq # manipulate JSON
-      yq # manipulate YAML
-      killall
-      lsof # print port process
-      nix-prefetch-git # get fetchgit hashes
-      nix-serve # create a local nix cachix like server
-      nurl # get fetchgit hashes
-      unrar
-      unzip
-      p7zip
-      wget # download http/ftp
-      ffmpeg # record, convert music/video
-      ffmpegthumbnailer
-      microfetch
-      imagemagick # image editor
-      scdl
-      yt-dlp
-      gtrash
-    ];
+    environment.systemPackages = with pkgs;
+      [
+        git
+        vim
+        curl
+        wget
+        file # print filetype
+        jq # manipulate JSON
+        yq # manipulate YAML
+        killall
+        lsof # print port process
+        nix-prefetch-git # get fetchgit hashes
+        nix-serve # create a local nix cachix like server
+        nurl # get fetchgit hashes
+        unrar
+        unzip
+        p7zip
+        wget # download http/ftp
+        ffmpeg # record, convert music/video
+        ffmpegthumbnailer
+        imagemagick # image editor
+        gtrash
+      ]
+      ++ lib.optionals config.bloat.enable [
+        microfetch
+        qemu
+        scdl
+        yt-dlp
+      ];
   };
 }

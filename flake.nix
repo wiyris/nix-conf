3dac79b6560d2db8a16e5fed37a9da8c81706793
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-10 15:34:13 +0900

    Fix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     nixosConfigurations = {
       shiki = mkNixosSystem inputs.nixpkgs "x86_64-linux" "shiki"; # desktop
       thiki = mkNixosSystem inputs.nixpkgs "x86_64-linux" "thiki"; # thinkpad
-      hiki = mkNixosSystem inputs.nixpkgs-stable "x86_64-linux" "hiki"; # homelab
+      hiki = mkNixosSystem inputs.nixpkgs "x86_64-linux" "hiki"; # homelab
 
       vm = mkNixosSystem inputs.nixpkgs "x86_64-linux" "vm"; # virtual machine
     };

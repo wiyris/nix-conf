7a13fe687945af4c5ee809ecaea7d264d988efcb
wiyris <wiyris.doctrine357@slmail.me>
2025-07-10 16:23:52 +0900

    tweak

--- hosts/hiki/networking.nix
+++ hosts/hiki/networking.nix
@@ -13,5 +13,19 @@
       address = "192.0.0.1";
       interface = "enp24s0";
     };
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+  };
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = ["~."];
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    dnsovertls = "true";
   };
 }

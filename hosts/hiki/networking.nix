a93a7e12352d87551bbc31be67474cc0c9276d93
wiyris <wiyris.doctrine357@slmail.me>
2025-07-10 16:46:36 +0900

    --

--- hosts/hiki/networking.nix
+++ hosts/hiki/networking.nix
@@ -1,32 +1,21 @@
 {
   networking = {
-    useDHCP = false;
     enableIPv6 = false;
     interfaces.enp24s0 = {
       ipv4.addresses = [
         {
-          address = "192.0.0.142";
+          address = "192.0.1.142";
           prefixLength = 24;
         }
       ];
     };
     defaultGateway = {
-      address = "192.0.0.1";
+      address = "192.0.1.1";
       interface = "enp24s0";
     };
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
     ];
-  };
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = ["~."];
-    fallbackDns = [
-      "1.1.1.1"
-      "1.0.0.1"
-    ];
-    dnsovertls = "true";
   };
 }

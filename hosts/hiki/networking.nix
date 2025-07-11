fa54abbbdf9adbbf79f0859ca835f39ab9cc80a6
wiyris <wiyris.doctrine357@slmail.me>
2025-07-12 00:40:30 +0900

    test

--- hosts/hiki/networking.nix
+++ hosts/hiki/networking.nix
@@ -1,11 +1,10 @@
 {
-  networking.networkmanager.enable = false;
   systemd.network.enable = true;
   systemd.network.networks."10-lan" = {
     matchConfig.Name = "enp24s0";
     networkConfig = {
       DHCP = "no";
-      Address = "192.168.0.142/16";
+      Address = "192.168.1.142/24";
       Gateway = "192.168.0.1";
       DNS = ["1.1.1.1" "1.0.0.1"];
     };

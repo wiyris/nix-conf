3e1ec31c47be6d34b8065b7aa23f87235de85910
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-12 01:00:27 +0900

    test

--- hosts/hiki/networking.nix
+++ hosts/hiki/networking.nix
@@ -1,12 +1,12 @@
 {
-  systemd.network.enable = true;
-  systemd.network.networks."10-lan" = {
-    matchConfig.Name = "enp24s0";
-    networkConfig = {
-      DHCP = "no";
-      Address = "192.168.1.142/24";
-      Gateway = "192.168.0.1";
-      DNS = ["1.1.1.1" "1.0.0.1"];
-    };
+  networking = {
+    interfaces.enp24s0.ipv4.addresses = [
+      {
+        address = "192.168.1.2";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = "192.168.1.1";
+    nameservers = ["1.1.1.1" "1.0.0.1"];
   };
 }

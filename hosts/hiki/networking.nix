33b2e4fabeaa9981f67454869b46f7af5cc9a1d7
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-12 20:15:12 +0900

    update

--- hosts/hiki/networking.nix
+++ hosts/hiki/networking.nix
@@ -2,11 +2,12 @@
   networking = {
     interfaces.enp24s0.ipv4.addresses = [
       {
-        address = "192.168.1.2";
+        address = "192.168.0.42";
         prefixLength = 24;
       }
     ];
-    defaultGateway = "192.168.1.1";
-    nameservers = ["1.1.1.1" "1.0.0.1"];
+    defaultGateway = "192.168.0.1";
+    nameservers = ["192.168.0.1"];
   };
+  services.openssh.enable = true;
 }

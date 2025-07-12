ac2a66bffbaf1704652ba59527de078ebb26c81e
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-12 22:30:04 +0900

    add scratchpad

--- modules/programs/gui/browser/chromium/default.nix
+++ modules/programs/gui/browser/chromium/default.nix
@@ -5,12 +5,26 @@
   ...
 }: let
   cfg = config.custom.programs.chromium;
+  kagi-scratchpad = pkgs.writeShellScriptBin "kagi-scratchpad" ''
+    #!/usr/bin/env bash
+    chromium --app="https://kagi.com/assistant"
+  '';
+  kagi-translate-scratchpad = pkgs.writeShellScriptBin "kagi-translate-scratchpad" ''
+    #!/usr/bin/env bash
+    chromium --app="https://translate.kagi.com/"
+  '';
 in {
   options.custom.programs.chromium.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm.programs.chromium = {
-      enable = true;
-      package = pkgs.ungoogled-chromium;
+    hm = {
+      home.package = [
+        kagi-scratchpad
+        kagi-translate-scratchpad
+      ];
+      programs.chromium = {
+        enable = true;
+        package = pkgs.ungoogled-chromium;
+      };
     };
   };
 }

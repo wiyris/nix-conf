0082bd299eb804c9b8e939f86ad17d6862313167
wiyris <wiyris.doctrine357@slmail.me>
2025-07-13 01:54:52 +0900

    increase timeout

--- hosts/hiki/default.nix
+++ hosts/hiki/default.nix
@@ -44,7 +44,7 @@ in {
 
     time.timeZone = "Asia/Tokyo";
 
-    boot.loader.timeout = 0;
+    boot.loader.timeout = 10;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     system.stateVersion = "25.05"; # Did you read the comment?

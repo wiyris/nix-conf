abd180d056d76a4c624d1d981f321775f7c129fb
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-18 00:43:15 +0900

    chore

--- modules/desktop/window-managers/hyprland/dots/misc.nix
+++ modules/desktop/window-managers/hyprland/dots/misc.nix
@@ -9,9 +9,6 @@
       no_donation_nag = true;
     };
 
-    # monitor = "DP-4,1280x960@143.85,0x0,1";
-
-    # monitor = "HDMI-A-1, 1920x1080@60, auto, 1";
     xwayland.force_zero_scaling = true;
     dwindle.force_split = 2;
   };

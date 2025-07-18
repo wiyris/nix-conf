16298b81be07b6fba6ade9a6a86a22ae6a78c726
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-19 00:45:25 +0900

    test

--- modules/programs/gui/browser/firefox/default.nix
+++ modules/programs/gui/browser/firefox/default.nix
@@ -13,30 +13,31 @@ in {
   options.custom.programs.firefox.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm = {
-      stylix.targets.firefox.profileNames = ["${userName}"];
-      programs.firefox = {
-        enable = true;
-        policies = import ./dots/policies.nix;
-        profiles.${userName} = {
-          search = import ./dots/search.nix;
-          extensions.packages = with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons; [
-            kagi-search
-            proton-pass
-            simplelogin
-            stylus
-            surfingkeys
-            ublock-origin
-          ];
-
-          extraConfig = ''
-            ${builtins.readFile "${betterfox}/user.js"}
-            ${builtins.readFile "${betterfox}/Fastfox.js"}
-            ${builtins.readFile "${betterfox}/Peskyfox.js"}
-            ${builtins.readFile "${betterfox}/Smoothfox.js"}
-            ${builtins.readFile myOverride}
-          '';
-        };
-      };
+      home.packages = [pkgs.firefox];
+      # stylix.targets.firefox.profileNames = ["${userName}"];
+      # programs.firefox = {
+      #   enable = true;
+      #   policies = import ./dots/policies.nix;
+      #   profiles.${userName} = {
+      #     search = import ./dots/search.nix;
+      #     extensions.packages = with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons; [
+      #       kagi-search
+      #       proton-pass
+      #       simplelogin
+      #       stylus
+      #       surfingkeys
+      #       ublock-origin
+      #     ];
+      #
+      #     extraConfig = ''
+      #       ${builtins.readFile "${betterfox}/user.js"}
+      #       ${builtins.readFile "${betterfox}/Fastfox.js"}
+      #       ${builtins.readFile "${betterfox}/Peskyfox.js"}
+      #       ${builtins.readFile "${betterfox}/Smoothfox.js"}
+      #       ${builtins.readFile myOverride}
+      #     '';
+      #   };
+      # };
     };
   };
 }

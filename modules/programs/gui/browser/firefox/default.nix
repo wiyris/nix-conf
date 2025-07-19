1427c4bcd661ed55b97fe84a3fe92a30df58822e
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-19 09:59:31 +0900

    ref

--- modules/programs/gui/browser/firefox/default.nix
+++ modules/programs/gui/browser/firefox/default.nix
@@ -10,34 +10,48 @@
   cfg = config.custom.programs.firefox;
   inherit (config.globals) userName;
 in {
-  options.custom.programs.firefox.enable = lib.mkEnableOption {};
-  config = lib.mkIf cfg.enable {
-    hm = {
-      home.packages = [pkgs.firefox];
-      # stylix.targets.firefox.profileNames = ["${userName}"];
-      # programs.firefox = {
-      #   enable = true;
-      #   policies = import ./dots/policies.nix;
-      #   profiles.${userName} = {
-      #     search = import ./dots/search.nix;
-      #     extensions.packages = with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons; [
-      #       kagi-search
-      #       proton-pass
-      #       simplelogin
-      #       stylus
-      #       surfingkeys
-      #       ublock-origin
-      #     ];
-      #
-      #     extraConfig = ''
-      #       ${builtins.readFile "${betterfox}/user.js"}
-      #       ${builtins.readFile "${betterfox}/Fastfox.js"}
-      #       ${builtins.readFile "${betterfox}/Peskyfox.js"}
-      #       ${builtins.readFile "${betterfox}/Smoothfox.js"}
-      #       ${builtins.readFile myOverride}
-      #     '';
-      #   };
-      # };
-    };
+  options.custom.programs.firefox.enable = {
+    enable = lib.mkEnableOption {};
+    isDefault = lib.mkEnableOption {};
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.isDefault {
+      globals.defaultBrowser = "zen-beta";
+      xdg.mimeApps.defaultApplications = {
+        "text/html" = "zen-beta.desktop";
+        "x-scheme-handler/http" = "zen-beta.desktop";
+        "x-scheme-handler/https" = "zen-beta.desktop";
+      };
+    })
+    (lib.mkIf cfg.enable {
+      hm = {
+        home.packages = [pkgs.firefox];
+        # stylix.targets.firefox.profileNames = ["${userName}"];
+        # programs.firefox = {
+        #   enable = true;
+        #   policies = import ./dots/policies.nix;
+        #   profiles.${userName} = {
+        #     search = import ./dots/search.nix;
+        #     extensions.packages = with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons; [
+        #       kagi-search
+        #       proton-pass
+        #       simplelogin
+        #       stylus
+        #       surfingkeys
+        #       ublock-origin
+        #     ];
+        #
+        #     extraConfig = ''
+        #       ${builtins.readFile "${betterfox}/user.js"}
+        #       ${builtins.readFile "${betterfox}/Fastfox.js"}
+        #       ${builtins.readFile "${betterfox}/Peskyfox.js"}
+        #       ${builtins.readFile "${betterfox}/Smoothfox.js"}
+        #       ${builtins.readFile myOverride}
+        #     '';
+        #   };
+        # };
+      };
+    })
+  ];
 }
--- modules/programs/gui/browser/zen-browser/default.nix
+++ modules/programs/gui/browser/zen-browser/default.nix
@@ -13,7 +13,14 @@ in {
     isDefault = lib.mkEnableOption {};
   };
   config = lib.mkMerge [
-    (lib.mkIf cfg.isDefault {globals.defaultBrowser = "zen-beta";})
+    (lib.mkIf cfg.isDefault {
+      globals.defaultBrowser = "zen-beta";
+      xdg.mimeApps.defaultApplications = {
+        "text/html" = "zen-beta.desktop";
+        "x-scheme-handler/http" = "zen-beta.desktop";
+        "x-scheme-handler/https" = "zen-beta.desktop";
+      };
+    })
     (lib.mkIf cfg.enable {
       environment.systemPackages = [inputs.zen-browser.packages."${pkgs.system}".default];
       hm = {
@@ -23,12 +30,6 @@ in {
           # extraConfig = ''
           #   ${builtins.readFile "${betterfox}/zen/user.js"}
           # '';
-        };
-
-        xdg.mimeApps.defaultApplications = {
-          "text/html" = "zen-beta.desktop";
-          "x-scheme-handler/http" = "zen-beta.desktop";
-          "x-scheme-handler/https" = "zen-beta.desktop";
         };
       };
     })

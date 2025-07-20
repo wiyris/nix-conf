9e0c7f8f9a230b3a21690ed20fa06bbdde19ce72
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-20 11:16:51 +0900

    ++

--- hosts/hiki/modules/lms/default.nix
+++ hosts/hiki/modules/lms/default.nix
@@ -2,6 +2,15 @@
   environment.systemPackages = [pkgs.lms];
   environment.etc."lms.conf".text = ''
   '';
+
+  users.users.lms = {
+    isSystemUser = true;
+    group = "lms";
+    home = "/var/lib/lms";
+    createHome = true;
+    shell = pkgs.shadow.bins.nologin;
+  };
+
   systemd.services.my-custom-service = {
     description = "Lightweight Music Server - Access your self-hosted music using a web interface";
     after = ["network.target"];
@@ -10,7 +19,9 @@
     serviceConfig = {
       Type = "simple";
       User = "lms";
-      ExecStart = "lms";
+      Group = "lms";
+      ExecStart = "${pkgs.lms}/bin/lms";
+      WorkingDirectory = "/var/lib/lms";
       Restart = "always";
       RestartSec = 30;
     };

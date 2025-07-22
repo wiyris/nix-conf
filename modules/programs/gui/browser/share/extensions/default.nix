626c268f187015be2ecd244e147ff0e8f3d86059
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-22 16:50:55 +0900

    test:

---
+++ modules/programs/gui/browser/share/extensions/default.nix
@@ -0,0 +1,6 @@
+{
+  "uBlock0@raymondhill.net" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/file/latest/ublock_origin/latest.xpi";
+    installation_mode = "force_installed";
+  };
+}

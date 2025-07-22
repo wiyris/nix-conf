4603ddebff9d4140726475a72a462b572026efa4
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-22 21:54:09 +0900

    Update:

--- modules/programs/gui/browser/share/policies/extensions/default.nix
+++ modules/programs/gui/browser/share/policies/extensions/default.nix
@@ -17,22 +17,22 @@
   };
   "search@kagi.com" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/kagi-search-for-firefox/latest.xpi";
-    # installation_mode = "force_installed";
+    installation_mode = "force_installed";
   };
   "{a8332c60-5b6d-41ee-bfc8-e9bb331d34ad}" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/surfingkeys_ff/latest.xpi";
-    # installation_mode = "force_installed";
+    installation_mode = "force_installed";
   };
   "ghosttext@bfred.it" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/ghosttext/latest.xpi";
-    # installation_mode = "force_installed";
+    installation_mode = "force_installed";
   };
   "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
-    # installation_mode = "force_installed";
+    installation_mode = "force_installed";
   };
   "{bbb880ce-43c9-47ae-b746-c3e0096c5b76}" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-web-file-icons/latest.xpi";
-    # installation_mode = "force_installed";
+    installation_mode = "force_installed";
   };
 }

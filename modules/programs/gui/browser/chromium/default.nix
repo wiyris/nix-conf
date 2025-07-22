2e084043ed7a9dbffd74e9f4e666dc6b806475e0
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-22 17:14:23 +0900

    Update todo

--- modules/programs/gui/browser/chromium/default.nix
+++ modules/programs/gui/browser/chromium/default.nix
@@ -8,8 +8,8 @@
 in {
   options.custom.programs.chromium.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    # setup ungoogled-chromium extensions w/ home-manager guide
-    # https://gist.github.com/MaximilianGaedig/acbce27522c997e9666bd93cef77492d
+    # TODO: setup ungoogled-chromium extensions w/ home-manager
+    # guide: https://gist.github.com/MaximilianGaedig/acbce27522c997e9666bd93cef77492d
     hm.programs.chromium = {
       enable = true;
       package = pkgs.ungoogled-chromium;

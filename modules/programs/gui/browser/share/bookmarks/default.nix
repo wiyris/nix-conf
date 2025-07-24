192a38b557276717ca483cd9b4ef8d8ae0db6014
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-24 22:30:45 +0900

    Update

--- modules/programs/gui/browser/share/bookmarks/default.nix
+++ modules/programs/gui/browser/share/bookmarks/default.nix
@@ -2,19 +2,51 @@
   force = true;
   settings = [
     {
+      name = "Box";
+      bookmarks = [
+        {
+          name = "Mailbox.org";
+          url = "https://office.mailbox.org/appsuite/#!!&app=io.ox/mail&folder=default0/INBOX&storeLocale=true";
+          tags = ["box" "mail"];
+          keyword = "mailbox";
+        }
+        {
+          name = "Proton Mail";
+          url = "https://mail.proton.me/";
+          tags = ["box" "mail" "proton"];
+          keyword = "protonmail";
+        }
+        {
+          name = "Proton Pass";
+          url = "https://pass.proton.me/";
+          tags = ["box" "password" "proton"];
+          keyword = "protonpass";
+        }
+        {
+          name = "Simble Login";
+          url = "https://app.simplelogin.io/dashboard/";
+          tags = ["box" "mail" "alias"];
+          keyword = "simplelogin";
+        }
+      ];
+    }
+    {
       name = "Nix";
       bookmarks = [
         {
           name = "Nix Manual";
           url = "https://nixos.org/manual/nix/stable/";
+          tags = ["nix" "docs"];
         }
         {
           name = "Nixpkgs Manual";
           url = "https://ryantm.github.io/nixpkgs/";
+          tags = ["nix" "docs"];
         }
         {
           name = "Noogle";
           url = "https://noogle.dev/";
+          tags = ["nix"];
         }
       ];
     }
@@ -37,6 +69,7 @@
         {
           name = "wallhaven";
           url = "https://wallhaven.cc/";
+          tags = "wallpaper";
         }
       ];
     }

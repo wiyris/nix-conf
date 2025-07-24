004afec6016733e8ae4320381cb4afa008741319
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-24 23:17:20 +0900

    Revert "Add keyboard shortcuts config"
    This reverts commit 28842e880dced58f66410a8d4889347d340c8cb2.

--- modules/programs/gui/browser/zen-browser/default.nix
+++ modules/programs/gui/browser/zen-browser/default.nix
@@ -36,8 +36,6 @@ in {
         home.file.".zen/${userName}/chrome" = catppuccin;
         home.file.".zen/${altUser}/chrome" = catppuccin;
 
-        home.file.".zen/${userName}/zen-keyboard-shortcuts" = ./dots/zen-keyboard-shortcuts.json;
-
         programs.zen-browser = {
           enable = true;
           policies = import ../share/policies;

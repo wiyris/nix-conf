10fbcd8dc68e3bf6529b59899767617f06d03a2c
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-27 08:08:19 +0900

    Define border color using stylix

--- modules/desktop/window-managers/hyprland/dots/general.nix
+++ modules/desktop/window-managers/hyprland/dots/general.nix
@@ -1,4 +1,8 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  ...
+}: {
   wayland.windowManager.hyprland.settings.general = {
     allow_tearing = true;
     gaps_in = 5;
@@ -7,9 +11,9 @@
     layout = "dwindle";
     resize_on_border = true;
 
-    "col.active_border" = lib.mkForce "rgba(f5e0dc69) rgba(eba0ac69) 45deg";
+    "col.active_border" = lib.mkForce "rgba(${config.lib.stylix.colors.base0E}69)";
+
+    # "col.active_border" = lib.mkForce "rgba(f5e0dc69) rgba(eba0ac69) 45deg";
     # "col.inactive_border" = lib.mkForce "rgba(f5e0dc17) rgba(eba0ac17) 45deg";
-    # "col.active_border" = "rgba(7fbbb3ee) 45deg";
-    # "col.inactive_border" = "rgba(333333ee) 45deg";
   };
 }

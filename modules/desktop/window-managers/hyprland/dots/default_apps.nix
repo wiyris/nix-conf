59a58be5c2b1d96e75726ec5df87010bb4bfa21d
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-27 08:43:42 +0900

    replace explorer command with global config

--- modules/desktop/window-managers/hyprland/dots/default_apps.nix
+++ modules/desktop/window-managers/hyprland/dots/default_apps.nix
@@ -9,6 +9,6 @@ in {
     "$bar" = "uwsm-app -- waybar";
     "$bar-toggle" = "killall -SIGUSR1 .waybar-wrapped";
     "$bar-reload" = "killall .waybar-wrapped; $bar";
-    "$explorer" = "thunar";
+    "$explorer" = "${defaultExplorer}";
   };
 }

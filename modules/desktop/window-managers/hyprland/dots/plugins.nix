12fc39cafeac3555ef332817bbd171ccb597db01
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-27 05:44:49 +0900

    Update

--- modules/desktop/window-managers/hyprland/dots/plugins.nix
+++ modules/desktop/window-managers/hyprland/dots/plugins.nix
@@ -1,6 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  rgb = color: "rgb(${color})";
+in {
   wayland.windowManager.hyprland = {
     plugins = [pkgs.hyprlandPlugins.hyprexpo];
-    bind = ["$mainMod, SLASH, hyprexpo:expo, toggle"];
+    settings = {
+      plugin.hyprexpo = {
+        columns = 3;
+        gap_size = 5;
+        bg_col = rgb config.lib.stylix.colors.base00;
+        workspace_method = "first 1";
+
+        enable_gesture = true;
+        gesture_fingers = 3;
+        gesture_distance = 300;
+        gesture_positive = false;
+      };
+      bindd = ["$mainMod, SLASH, Open expose, hyprexpo:expo, toggle"];
+    };
   };
 }

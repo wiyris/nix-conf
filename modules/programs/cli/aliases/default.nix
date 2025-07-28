92ee3ac8c5f9d0f0c5a6ad14a5493ce525b655bf
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-07-28 23:13:13 +0900

    Move abbrs loc

--- modules/programs/cli/aliases/default.nix
+++ modules/programs/cli/aliases/default.nix
@@ -27,9 +27,5 @@ in {
       ".4" = "cd ../.../../..";
       ".5" = "cd ../../../../..";
     };
-    hm.programs.fish.shellAbbrs = {
-      kan = "killall -9";
-      xx = "7z";
-    };
   };
 }
--- modules/programs/cli/fish/dots/abbrs.nix
+++ modules/programs/cli/fish/dots/abbrs.nix
@@ -1,6 +1,8 @@
 {
   programs.fish = {
     shellAbbrs = {
+      kan = "killall -9";
+      xx = "7z";
       vol = "wpctl set-volume '@DEFAULT_AUDIO_SINK@'";
       yt = "yt-dlp";
     };

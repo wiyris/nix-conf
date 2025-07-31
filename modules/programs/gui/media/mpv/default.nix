e686877dee6f18bb808fb80191e63ecb3dca8d2c
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-01 01:11:55 +0900

    feat(mvp):Add config and scripts

--- modules/programs/gui/media/mpv/default.nix
+++ modules/programs/gui/media/mpv/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }: let
@@ -7,6 +8,30 @@
 in {
   options.custom.programs.mpv.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm.programs.mpv.enable = true;
+    hm.programs.mpv = {
+      enable = true;
+      config = {
+        osc = false;
+        border = false;
+
+        autofit-larger = "75%x75%";
+        hwdec-codecs = "all";
+        keep-open = true;
+        pause = true;
+        video-sync = "display-resample";
+
+        # enable hardware acceleration
+        hwdec = "auto-safe";
+        vo = "gpu";
+      };
+      scripts = with pkgs; [
+        mpvScripts.mpris
+        mpvScripts.thumbfast
+        mpvScripts.uosc
+        mpvScripts.cutter
+        mpvScripts.quality-menu
+        mpvScripts.mpv-cheatsheet
+      ];
+    };
   };
 }

1105ba1b64f5356196754e44855d06e8e13aa64b
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-01 16:13:12 +0900

    chore(binds):update

--- modules/desktop/wm/hyprland/dots/binds.nix
+++ modules/desktop/wm/hyprland/dots/binds.nix
@@ -7,7 +7,7 @@
       "$mainMod, BackSpace, exec, uwsm-app -- $terminal -e nvim"
       "$mainMod, J, exec, uwsm-app -- $browser"
       "$mainMod SHIFT, J, exec, uwsm-app -- $browser -p skw"
-      "$mainMod, M, exec, uwsm-app -- $explorer"
+      # "$mainMod, M, exec, uwsm-app -- $explorer"
 
       # Rofi
       "$mainMod, T, exec, $launcher"

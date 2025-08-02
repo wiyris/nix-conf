39f64ed0a3e0895ec3f22ef6818cd3fd37d3cd5e
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-02 14:55:29 +0900

    chore(hypr):update exec-onec

--- modules/desktop/wm/hyprland/dots/exec-once.nix
+++ modules/desktop/wm/hyprland/dots/exec-once.nix
@@ -1,11 +1,12 @@
 {
   wayland.windowManager.hyprland.settings.exec-once = [
     # "fcitx5"
-    # "$bar"
-    "steam"
+    "$bar"
+    "uwsm-app -- steam"
     # "spotify"
-    "obsidian"
-    "thunderbird"
+    "uwsm-app -- thunar --daemon"
+    "uwsm-app -- obsidian"
+    "uwsm-app -- thunderbird"
     # "gsettings set org.gnome.desktop.interface cursor-theme rose-pine-hyprcursor"
     # "gsettings set org.gnome.desktop.interface cursor-size 32"
   ];

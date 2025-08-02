2ea9bc3e2fbd3e2c0beaeb3c6f1a328c7e91046c
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-02 13:50:45 +0900

    feat(gamemode):add gamemode config

--- modules/core/user/default.nix
+++ modules/core/user/default.nix
@@ -4,7 +4,7 @@
     custom.user = {
       hashedPassword = "$y$j9T$E0yTqNdhAhjJLH4MU6eLK.$zEqoByk1W/aW.9XRtFJqVCxsF3DZnqS8iHOL9VGU2DD";
       isNormalUser = true;
-      extraGroups = ["wheel"];
+      extraGroups = ["wheel" "gamemode"];
       shell = pkgs.fish;
     };
   };
--- modules/desktop/programs/gaming/optimize/default.nix
+++ modules/desktop/programs/gaming/optimize/default.nix
@@ -22,6 +22,18 @@ in {
     programs.gamemode = {
       enable = true;
       settings = {
+        general = {
+          renice = 19;
+        };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 0;
+          amd_performance_level = "high";
+        };
+        # custom = {
+        #   start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        #   end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        # };
       };
     };
 

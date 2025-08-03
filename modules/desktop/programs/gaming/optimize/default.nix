11901d46eeb3e9da32d75d7a5c9eb720cb5a5fe6
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-03 12:54:19 +0900

    chore(gaming):

--- modules/desktop/programs/gaming/optimize/default.nix
+++ modules/desktop/programs/gaming/optimize/default.nix
@@ -14,36 +14,44 @@ in {
   ];
   options.gaming.optimize.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
+    chaotic.nyx.overlay.enable = lib.mkDefault true;
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
-    programs.steam.platformOptimizations.enable = true;
     chaotic.mesa-git.enable = true; # for anti lag 2
-
-    programs.gamemode = {
-      enable = true;
-      settings = {
-        general = {
-          renice = 19;
+    programs = {
+      gamescope = {
+        enable = true;
+        package = pkgs.gamescope_git;
+      };
+      gamemode = {
+        enable = true;
+        settings = {
+          general = {
+            renice = 19;
+          };
+          gpu = {
+            apply_gpu_optimisations = "accept-responsibility";
+            gpu_device = 0;
+            amd_performance_level = "high";
+          };
+          # custom = {
+          #   start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          #   end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+          # };
         };
-        gpu = {
-          apply_gpu_optimisations = "accept-responsibility";
-          gpu_device = 0;
-          amd_performance_level = "high";
+      };
+      steam = {
+        platformOptimizations.enable = true;
+        gamescopeSession = {
+          enable = true;
         };
-        # custom = {
-        #   start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-        #   end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-        # };
       };
     };
 
     services.pipewire.lowLatency = {
-      # enable this module
       enable = true;
-      # defaults (no need to be set unless modified)
       quantum = 64;
       rate = 48000;
     };
-
     # make pipewire realtime-capable
     security.rtkit.enable = true;
   };

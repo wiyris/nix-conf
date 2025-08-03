67b0a00c41493598d31e2b06801f3e5d598909e8
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-03 18:17:48 +0900

    chore(tmux): update config

--- modules/core/programs/tmux/default.nix
+++ modules/core/programs/tmux/default.nix
@@ -12,6 +12,10 @@ in {
   config = lib.mkIf cfg.enable {
     hm = {
       stylix.targets.tmux.enable = false;
+      imports = [
+        ./dots/binds.nix
+        ./plugins/catppuccin.nix
+      ];
       programs.tmux = {
         enable = true;
         inherit shell;
@@ -19,17 +23,8 @@ in {
         escapeTime = 0;
         aggressiveResize = true;
         terminal = "screen-256color";
-
-        imports = [
-          ./dots/binds.nix
-          ./plugins/catppuccin.nix
-        ];
-
         extraConfig = ''
           set -g status-position top
-          set -Fg 'status-format[1]' '#{status-format[0]}'
-          set -g 'status-format[1]' ""
-          set -g status 2
         '';
         plugins = with pkgs.tmuxPlugins; [
           tmux-fzf
--- modules/core/programs/tmux/plugins/catppuccin.nix
+++ modules/core/programs/tmux/plugins/catppuccin.nix
@@ -3,39 +3,18 @@
     plugins = with pkgs.tmuxPlugins; [catppuccin];
     extraConfig = ''
       set -g @catppuccin_flavor 'mocha'
-      set -g status-right-length 100
-      set -g status-left ""
-
-      # Window
-      set -g @catppuccin_window_status_style "custom"
-      set -g window-status-separator ""
-
-      ## Window global/default configuration
-      set -g @catppuccin_window_default_text " #{window_name}"
-      set -g @catppuccin_window_status "icon"
-      set -g @catppuccin_window_default_fill "number"
-      set -g @catppuccin_window_number_position "left"
-
-      set -g @catppuccin_window_left_separator "█"
-      set -g @catppuccin_window_middle_separator "█"
-      set -g @catppuccin_window_right_separator "█"
-
-      ## Window current configuration
-      set -g @catppuccin_window_current_text "#{window_name}"
-      set -g @catppuccin_window_current_fill "all"
-      set -g @catppuccin_window_current_middle_separator "#[reverse] 󰿟 #[noreverse]"
-
-      # Status modules config
-      set -g @catppuccin_date_time_text "%d-%m %H:%M"
-
+      set -g @catppuccin_window_status_style "rounded"
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
 
-      set -gF  status-right "#{@catppuccin_status_directory}"
-      set -agF status-right "#{@catppuccin_status_session}"
-      set -agF status-right "#{@catppuccin_status_user}"
-      set -agF status-right "#{@catppuccin_status_host}"
-      set -agF status-right "#{E:@catppuccin_status_date_time}"
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+      set -agF status-right "#{E:@catppuccin_status_battery}"
     '';
   };
 }

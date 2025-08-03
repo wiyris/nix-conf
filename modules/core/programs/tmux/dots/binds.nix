64ad129835b0430108752bdc782d3d738e92a88e
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-03 21:49:58 +0900

    chore(tmux): update binds

--- modules/core/programs/tmux/dots/binds.nix
+++ modules/core/programs/tmux/dots/binds.nix
@@ -3,6 +3,9 @@
     keyMode = "vi";
     prefix = "M-Space";
     extraConfig = ''
+      # Config reload
+      bind C-r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded"
+      bind 'v' copy-mode
     '';
   };
 }

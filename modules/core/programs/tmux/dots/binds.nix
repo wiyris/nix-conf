e362954a79ad7519efbae1f92e53cc6b460668fa
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-04 18:57:38 +0900

    binds(tmux): fix config reload bind

--- modules/core/programs/tmux/dots/binds.nix
+++ modules/core/programs/tmux/dots/binds.nix
@@ -4,7 +4,7 @@
     prefix = "M-Space";
     extraConfig = ''
       # Config reload
-      bind C-r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded"
+      bind M-r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded"
       bind 'v' copy-mode
     '';
   };

dca71688667ee88af07f007c8af19e2af949c933
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-05 18:21:12 +0900

    fix(hypr): prevent autostart hypridle on other wayland session

--- modules/desktop/services/hypridle/default.nix
+++ modules/desktop/services/hypridle/default.nix
@@ -7,36 +7,39 @@
 in {
   options.desktop.hypridle.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm.services.hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on";
-          ignore_dbus_inhibit = false;
-          lock_cmd = "pidof hyprlock || hyprlock";
-        };
+    hm = {
+      systemd.user.services.hypridle.Install.WantedBy = lib.mkForce [];
+      services.hypridle = {
+        enable = true;
+        settings = {
+          general = {
+            before_sleep_cmd = "loginctl lock-session";
+            after_sleep_cmd = "hyprctl dispatch dpms on";
+            ignore_dbus_inhibit = false;
+            lock_cmd = "pidof hyprlock || hyprlock";
+          };
 
-        listener = [
-          {
-            timeout = 180;
-            on-timeout = "brightnessctl -s set 30";
-            on-resume = "brightnessctl -r";
-          }
-          {
-            timeout = 3600;
-            on-timeout = "loginctl lock-session";
-          }
-          # {
-          #   timeout = 1800;
-          #   on-timeout = "hyprctl dispatch dpms off";
-          #   on-resume = "hyprctl dispatch dpms on";
-          # }
-          # {
-          #   timeout = 3600;
-          #   on-timeout = "sysemctl suspend";
-          # }
-        ];
+          listener = [
+            {
+              timeout = 180;
+              on-timeout = "brightnessctl -s set 30";
+              on-resume = "brightnessctl -r";
+            }
+            {
+              timeout = 3600;
+              on-timeout = "loginctl lock-session";
+            }
+            # {
+            #   timeout = 1800;
+            #   on-timeout = "hyprctl dispatch dpms off";
+            #   on-resume = "hyprctl dispatch dpms on";
+            # }
+            # {
+            #   timeout = 3600;
+            #   on-timeout = "sysemctl suspend";
+            # }
+          ];
+        };
       };
     };
   };

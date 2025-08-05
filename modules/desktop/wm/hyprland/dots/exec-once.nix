8cba02ed0fff1782149388ef614ec6698e7e85bd
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-05 13:27:17 +0900

    chore(hypr): enable thunar daemon

--- modules/desktop/wm/hyprland/dots/exec-once.nix
+++ modules/desktop/wm/hyprland/dots/exec-once.nix
@@ -1,10 +1,11 @@
 {
   wayland.windowManager.hyprland.settings.exec-once = [
+    "uwsm finalize XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP"
     # "fcitx5"
     "$bar"
     # "uwsm app -- steam"
     # "spotify"
-    # "uwsm app -- thunar --daemon"
+    "uwsm app -- thunar --daemon"
     # "uwsm app -- obsidian"
     # "uwsm app -- thunderbird"
     # "gsettings set org.gnome.desktop.interface cursor-theme rose-pine-hyprcursor"

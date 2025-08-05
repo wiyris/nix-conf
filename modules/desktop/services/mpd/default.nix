3406f49e696f4b2d2e5a8126a4c29c8d63249e6d
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-05 23:40:50 +0900

    chore(mpd): enable mpd-mpris

--- modules/desktop/services/mpd/default.nix
+++ modules/desktop/services/mpd/default.nix
@@ -9,7 +9,7 @@ in {
   options.desktop.mpd.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm = {
-      # services.mpd-mpris.enable = true;
+      services.mpd-mpris.enable = true;
       services.playerctld.enable = true;
       services.mpd = {
         enable = true;

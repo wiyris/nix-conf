ea1a70bf07f7bbbed1d193b91cb28e585d3c654c
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-05 15:55:45 +0900

    chore(hypr): update decoration config

--- modules/desktop/wm/hyprland/dots/decoration.nix
+++ modules/desktop/wm/hyprland/dots/decoration.nix
@@ -8,19 +8,24 @@
 
     blur = {
       enabled = true;
-      size = 2;
+      size = 5;
       passes = 3;
       ignore_opacity = true;
-      noise = 0.0;
+      noise = 0.02;
       xray = false;
       popups = true;
+      popups_ignorealpha = 0.2;
       new_optimizations = true;
+      brightness = 0.9;
+      vibrancy = 0.1696;
+      vibrancy_darkness = 0.0;
+      input_methods = true;
     };
 
     shadow = {
-      enabled = false;
-      range = 45;
-      render_power = 10;
+      enabled = true;
+      range = 42;
+      render_power = 5;
       ignore_window = true;
       scale = 10;
       # color = lib.mkForce "rgba(f5e0dca7)";

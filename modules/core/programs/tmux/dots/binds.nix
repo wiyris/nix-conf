c320167aa76dc5b052d51089ef05fad7ab5cef73
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-06 11:36:02 +0900

    chore(tmux): update keybinds

--- modules/core/programs/tmux/dots/binds.nix
+++ modules/core/programs/tmux/dots/binds.nix
@@ -17,15 +17,18 @@
       bind -n M-Space split-window -h
       bind -n M-C-Space split-window -v
 
-      bind -n M-f if-shell 'tmux list-windows | grep -q "^1:"' 'select-window -t 1' 'new-window -t 1'
-      bind -n M-p if-shell 'tmux list-windows | grep -q "^2:"' 'select-window -t 2' 'new-window -t 2'
-      bind -n M-d if-shell 'tmux list-windows | grep -q "^3:"' 'select-window -t 3' 'new-window -t 3'
-      bind -n M-l if-shell 'tmux list-windows | grep -q "^4:"' 'select-window -t 4' 'new-window -t 4'
-      bind -n M-x if-shell 'tmux list-windows | grep -q "^5:"' 'select-window -t 5' 'new-window -t 5'
-      bind -n M-";" if-shell 'tmux list-windows | grep -q "^6:"' 'select-window -t 6' 'new-window -t 6'
-      bind -n M-u if-shell 'tmux list-windows | grep -q "^7:"' 'select-window -t 7' 'new-window -t 7'
-      bind -n M-o if-shell 'tmux list-windows | grep -q "^8:"' 'select-window -t 8' 'new-window -t 8'
-      bind -n M-y if-shell 'tmux list-windows | grep -q "^9:"' 'select-window -t 9' 'new-window -t 9'
+      bind -n M-n if-shell 'tmux list-windows | grep -q "^1:"' 'select-window -t 1' 'new-window -t 1'
+      bind -n M-t if-shell 'tmux list-windows | grep -q "^2:"' 'select-window -t 2' 'new-window -t 2'
+      bind -n M-h if-shell 'tmux list-windows | grep -q "^3:"' 'select-window -t 3' 'new-window -t 3'
+      # bind -n M-f if-shell 'tmux list-windows | grep -q "^1:"' 'select-window -t 1' 'new-window -t 1'
+      # bind -n M-p if-shell 'tmux list-windows | grep -q "^2:"' 'select-window -t 2' 'new-window -t 2'
+      # bind -n M-d if-shell 'tmux list-windows | grep -q "^3:"' 'select-window -t 3' 'new-window -t 3'
+      # bind -n M-l if-shell 'tmux list-windows | grep -q "^4:"' 'select-window -t 4' 'new-window -t 4'
+      # bind -n M-x if-shell 'tmux list-windows | grep -q "^5:"' 'select-window -t 5' 'new-window -t 5'
+      # bind -n M-";" if-shell 'tmux list-windows | grep -q "^6:"' 'select-window -t 6' 'new-window -t 6'
+      # bind -n M-u if-shell 'tmux list-windows | grep -q "^7:"' 'select-window -t 7' 'new-window -t 7'
+      # bind -n M-o if-shell 'tmux list-windows | grep -q "^8:"' 'select-window -t 8' 'new-window -t 8'
+      # bind -n M-y if-shell 'tmux list-windows | grep -q "^9:"' 'select-window -t 9' 'new-window -t 9'
 
       # Move to each pane
       bind -n M-i select-pane -R

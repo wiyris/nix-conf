a9706f840d3197c4a8efbca2d27dfd967b5034e1
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-10 19:23:43 +0900

    anki: update config

--- modules/desktop/programs/general/anki/default.nix
+++ modules/desktop/programs/general/anki/default.nix
@@ -8,10 +8,20 @@
 in {
   options.custom.programs.anki.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm.programs.anki = {
-      enable = true;
-      language = "en_US";
-      # addons = with pkgs.ankiAddons; [recolor];
+    hm = {
+      imports = [./binds.nix];
+      programs.anki = {
+        enable = true;
+        language = "en_US";
+        videoDriver = "vulkan";
+        # addons = with pkgs.ankiAddons; [recolor];
+        sync = {
+          autoSync = true;
+          syncMedia = true;
+          username = "36a8f759-6f5e-409c-80ed-3e8a3c606c9d@simplelogin.com";
+          # passwordFile = "";
+        };
+      };
     };
   };
 }

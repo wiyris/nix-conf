b58f6c0e1c49cfd3297a9568dec9a24ef68b9759
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-12 18:55:57 +0900

    fix(anki): stop letting home-manager manage anki

--- modules/desktop/programs/general/anki/default.nix
+++ modules/desktop/programs/general/anki/default.nix
@@ -1,7 +1,8 @@
 {
-  pkgs,
-  config,
   lib,
+  pkgs,
+  inputs,
+  config,
   ...
 }: let
   cfg = config.custom.programs.anki;
@@ -9,19 +10,20 @@ in {
   options.custom.programs.anki.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm = {
-      imports = [./binds.nix];
-      programs.anki = {
-        enable = true;
-        language = "en_US";
-        videoDriver = "vulkan";
-        # addons = with pkgs.ankiAddons; [recolor];
-        sync = {
-          autoSync = true;
-          syncMedia = true;
-          username = "36a8f759-6f5e-409c-80ed-3e8a3c606c9d@simplelogin.com";
-          # passwordFile = "";
-        };
-      };
+      home.packages = [pkgs.anki-bin];
+      # imports = [./binds.nix];
+      # programs.anki = {
+      #   enable = true;
+      #   language = "en_US";
+      #   videoDriver = "vulkan";
+      #   # addons = with pkgs.ankiAddons; [recolor];
+      #   sync = {
+      #     autoSync = true;
+      #     syncMedia = true;
+      #     username = "";
+      #     passwordFile = "";
+      #   };
+      # };
     };
   };
 }

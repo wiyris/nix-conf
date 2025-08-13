13d520bc9d3540f69607425959348f5f05c92c2a
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-13 20:44:39 +0900

    yazi: update config

---
+++ hosts/shiki/nix-ld.nix
@@ -0,0 +1,9 @@
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}: {
+  environment.systemPackages = with inputs.nix-alien.packages.${system}; [nix-alien];
+  programs.nix-ld.enable = true;
+}
--- modules/core/programs/yazi/default.nix
+++ modules/core/programs/yazi/default.nix
@@ -1,31 +1,79 @@
 {
-  config,
-  pkgs,
   lib,
+  pkgs,
+  config,
   ...
 }: let
   cfg = config.custom.programs.yazi;
 in {
   options.custom.programs.yazi.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      ffmpeg
-      ffmpegthumbnailer
-      p7zip
-      jq
-      fd
-      ripgrep
-      fzf
-      zoxide
-      resvg
-      imagemagick
-    ];
     hm = {
-      imports = [./plugins/git];
+      home.packages = with pkgs; [
+        ffmpeg
+        ffmpegthumbnailer
+        p7zip
+        jq
+        fd
+        ripgrep
+        fzf
+        zoxide
+        resvg
+        imagemagick
+      ];
       programs.yazi = {
         enable = true;
         enableFishIntegration = true;
         shellWrapperName = "y";
+        plugins = {
+          git = pkgs.yaziPlugins.git;
+          starship = pkgs.yaziPlugins.starship;
+          full-border = pkgs.yaziPlugins.full-border;
+        };
+        settings = {
+          mgr = {
+            layout = [1 4 3];
+            sort_by = "natural";
+            sort_sensitive = false;
+            sort_reverse = false;
+            sort_dir_first = true;
+            linemode = "none";
+            show_hidden = false;
+            show_symlink = true;
+          };
+          preview = {
+            tab_size = 2;
+            max_width = 600;
+            max_height = 900;
+          };
+          plugin.prepend_fetchers = [
+            {
+              id = "git";
+              name = "*";
+              run = "git";
+            }
+            {
+              id = "git";
+              name = "*/";
+              run = "git";
+            }
+          ];
+        };
+        initLua =
+          # lua
+          ''
+            require("git"):setup()
+            require("starship"):setup()
+            require("full-border"):setup({
+              type = ui.Border.ROUNDED,
+            })
+            require("zoxide"):setup({
+              update_db = false,
+            })
+            require("session"):setup({
+              sync_yanked = true,
+            })
+          '';
       };
     };
   };

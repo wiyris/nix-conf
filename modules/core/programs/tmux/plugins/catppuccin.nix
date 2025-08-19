65fa3a303281716676b2040af1e176a0712e8811
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-19 16:29:34 +0900

    tmux: update config

--- modules/core/programs/tmux/plugins/catppuccin.nix
+++ modules/core/programs/tmux/plugins/catppuccin.nix
@@ -3,7 +3,6 @@
     plugins = with pkgs.tmuxPlugins; [catppuccin];
     extraConfig = ''
       set -g @catppuccin_flavor 'mocha'
-      set -g @catppuccin_window_status_style "rounded"
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
 
@@ -11,10 +10,8 @@
       set -g status-left-length 100
       set -g status-left ""
       set -g status-right "#{E:@catppuccin_status_application}"
-      set -agF status-right "#{E:@catppuccin_status_cpu}"
       set -ag status-right "#{E:@catppuccin_status_session}"
       set -ag status-right "#{E:@catppuccin_status_uptime}"
-      set -agF status-right "#{E:@catppuccin_status_battery}"
     '';
   };
 }

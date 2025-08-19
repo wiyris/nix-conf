b8d8cf827ea5dffb15d585568f7d1ef7fe8683d2
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-19 19:47:26 +0900

    chore(thiki): update profile

--- hosts/thiki/default.nix
+++ hosts/thiki/default.nix
@@ -16,115 +16,86 @@ in {
     globals.homeDirectory = "/home/${userName}";
     globals.configDirectory = configDirectory;
 
-    networking.hostName = "thiki";
-
-    custom.stylix.enable = true;
-
+    # Custom options
     system.terminus-font.enable = true;
     custom.networking.mullvad.enable = true;
     custom.networking.core.enable = true;
-    custom.networking.iwd.enable = true;
-    # custom.networking.networkmanager.enable = true;
     custom.hardware.amdgpu.enable = true;
     custom.hardware.bluetooth.enable = true;
-    # custom.services.batsignal.enable = true;
-    # custom.services.keyd.enable = true;
-    # custom.services.hiki-mounts.enable = true;
-    # custom.services.rtorrent.enable = true;
-    # custom.services.sk-serve.enable = true;
-    # custom.services.tlp.enable = true;
 
     # cli
     programs.fish.enable = true;
     custom.programs.fish.enable = true;
     custom.programs.fish.isDefault = true;
 
-    custom.programs.aria2.enable = true;
     custom.programs.aliases.enable = true;
     custom.programs.bat.enable = true;
+    custom.programs.btop.enable = true;
     custom.programs.core-utils.enable = true;
     custom.programs.eza.enable = true;
     custom.programs.fastfetch.enable = true;
     custom.programs.fzf.enable = true;
     custom.programs.git.enable = true;
-    custom.programs.ripgrep.enable = true;
-    custom.programs.starship.enable = true;
-    custom.programs.zoxide.enable = true;
-
-    # tui
-    custom.programs.btop.enable = true;
-    custom.programs.cava.enable = true;
     custom.programs.gtrash.enable = true;
     custom.programs.htop.enable = true;
     custom.programs.lazygit.enable = true;
     custom.programs.nvim.enable = true;
+    custom.programs.ripgrep.enable = true;
+    custom.programs.starship.enable = true;
     custom.programs.tmux.enable = true;
     custom.programs.yazi.enable = true;
+    custom.programs.zoxide.enable = true;
 
-    # gui
-    custom.programs.ghostty.isDefault = true; # foot, ghostty, kitty
-    custom.programs.zen-browser.isDefault = true; # firefox, librewolf, zen-browser
-    custom.programs.thunar.isDefault = true; # thunar, dolphin
-
-    custom.programs.foot.enable = true;
-    # custom.programs.ghostty.enable = true;
-    # custom.programs.kitty.enable = true;
-
-    # custom.programs.chromium.enable = true;
-    # custom.programs.firefox.enable = true;
-    # custom.programs.schizofox.enable = true;
-    # custom.programs.librewolf.enable = true;
-    # custom.programs.mullvad-browser.enable = true;
-    # custom.programs.qutebrowser.enable = true;
-    # custom.programs.tor-browser.enable = true;
-    custom.programs.zen-browser.enable = true;
-
-    # custom.programs.dolphin.enable = true;
-    custom.programs.thunar.enable = true;
-
-    # custom.programs.freetube.enable = true;
-    # custom.programs.mpv.enable = true;
-    # custom.programs.pqiv.enable = true;
-    # custom.programs.rmpc.enable = true;
-    # custom.programs.spotify.enable = true;
-    # custom.programs.zathura.enable = true;
-
-    # custom.programs.anki.enable = true;
-    # custom.programs.goldendict.enable = true;
-
-    # gaming
-    # gaming.lutris.enable = true;
-    # gaming.osu.enable = true;
-    # gaming.optimize.enable = true;
-    # gaming.steam.enable = true;
-
-    # window-managers
+    # DESKTOP
+    ## window-managers
     desktop.hyprland.isDefault = true; # dwl, hyprland, niri
+    desktop.hyprland.enable = true;
 
-    # wm-addons
+    ## core
     desktop.clipboard.enable = true;
-    desktop.dunst.enable = true;
-    # desktop.fcitx.enable = true;
+    desktop.fcitx.enable = true;
     desktop.fonts.enable = true;
     desktop.gtk.enable = true;
-    desktop.hypridle.enable = true;
-    desktop.hyprlock.enable = true;
     desktop.mime-apps.enable = true;
-    desktop.rofi.enable = true;
-    # desktop.swayosd.enable = true;
-    # desktop.swww.enable = true;
     desktop.uwsm.enable = true;
-    desktop.waybar.enable = true;
     desktop.wayland-session.enable = true;
     desktop.wayland-utils.enable = true;
     desktop.xdg.enable = true;
 
-    # desktop.dwl.enable = true;
-    desktop.hyprland.enable = true;
-    # desktop.niri.enable = true;
+    ## programs
+    ### default programs
+    custom.programs.foot.isDefault = true; # foot, ghostty, kitty
+    custom.programs.zen-browser.isDefault = true; # firefox, librewolf, zen-browser
+    custom.programs.thunar.isDefault = true; # thunar, dolphin
 
-    # laptop.enable = true; # enable laptop modules
-    # extraPackages.enable = true; # add extra packages
+    ## general
+    custom.programs.hyprlock.enable = true;
+    custom.programs.rofi.enable = true;
+    custom.programs.tofi.enable = true;
+    custom.programs.taskwarrior.enable = true;
+
+    ## terminal emulator
+    custom.programs.foot.enable = true;
+
+    ## browser
+    custom.programs.zen-browser.enable = true;
+
+    ## exprorer
+    custom.programs.thunar.enable = true;
+
+    ## media
+    custom.programs.freetube.enable = true;
+    custom.programs.mpv.enable = true;
+    custom.programs.pqiv.enable = true;
+    custom.programs.zathura.enable = true;
+
+    ## services
+    custom.services.dunst.enable = true;
+    custom.services.hypridle.enable = true;
+    custom.services.pipewire.enable = true;
+    custom.services.swww.enable = true;
+    custom.services.waybar.enable = true;
+    laptop.enable = true; # enable laptop modules
 
     time.timeZone = "Asia/Tokyo";
 

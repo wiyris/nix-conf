196f8f1083420622d85bc5087e3ef81c3dcc0a90
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-19 21:33:19 +0900

    test(thiki): enable keyd

--- modules/core/services/keyd/us.nix
+++ modules/core/services/keyd/us.nix
@@ -9,50 +9,47 @@ in {
       "default_layout" = "default";
     };
     "default:layout" = {
-      capslock = "lettermod(meta, s, ${toString holdtime}, ${toString taptime})";
-      a = "lettermod(alt, n, ${toString holdtime}, ${toString taptime})";
-      s = "lettermod(control, t, ${toString holdtime}, ${toString taptime})";
-      d = "lettermod(shift, h, ${toString holdtime}, ${toString taptime})";
-      j = "lettermod(shift, a, ${toString holdtime}, ${toString taptime})";
-      k = "lettermod(control, e, ${toString holdtime}, ${toString taptime})";
-      l = "lettermod(alt, i, ${toString holdtime}, ${toString taptime})";
-      ";" = "lettermod(meta, c, ${toString holdtime}, ${toString taptime})";
+      # a = "lettermod(meta, s, ${toString holdtime}, ${toString taptime})";
+      # s = "lettermod(alt, n, ${toString holdtime}, ${toString taptime})";
+      # d = "lettermod(control, t, ${toString holdtime}, ${toString taptime})";
+      # f = "lettermod(shift, h, ${toString holdtime}, ${toString taptime})";
+      # j = "lettermod(shift, a, ${toString holdtime}, ${toString taptime})";
+      # k = "lettermod(control, e, ${toString holdtime}, ${toString taptime})";
+      # l = "lettermod(alt, i, ${toString holdtime}, ${toString taptime})";
+      # ";" = "lettermod(meta, c, ${toString holdtime}, ${toString taptime})";
 
-      leftshift = "lettermod(shift, v, ${toString holdtime}, ${toString taptime})";
+      capslock = "lettermod(control, r, ${toString holdtime}, ${toString taptime})";
+
       space = "overloadt(extend, space, ${toString taptime})";
       "q+w" = "q";
       "w+e" = "z";
       "s+d" = "esc";
       "a+d" = "C-space"; # toggle fcitx
       "j+k" = "S-;"; # :
-      "j+l" = "S-'"; # "
       "o+p" = "S-2"; # @
 
-      "a+s" = "tab";
-      "k+l" = "backspace";
-
-      "z+x" = "C-insert"; # copy
-      "x+c" = "S-insert"; # paste
-
-      "space+tab" = "S-`"; # ~
-      "space+q" = "S-0"; # )
-      "space+w" = "S-]"; # }
-      "space+e" = "]";
-
-      "space+u" = "backslash"; # \
-      "space+i" = "S-5"; # %
-      "space+o" = "S-6"; # ^
-      "space+p" = "`";
-
-      "space+capslock" = "S-backslash"; # |
-      "space+a" = "S-9"; # (
-      "space+s" = "S-["; # {
-      "space+d" = "[";
-
-      "space+j" = "S-8"; # \
-      "space+k" = "S-3"; # #
-      "space+l" = "S-5"; # %
-      "space+;" = "S-4"; # $
+      # "z+x" = "C-insert"; # copy
+      # "x+c" = "S-insert"; # paste
+      #
+      # "space+tab" = "S-`"; # ~
+      # "space+q" = "S-0"; # )
+      # "space+w" = "S-]"; # }
+      # "space+e" = "]";
+      #
+      # "space+u" = "backslash"; # \
+      # "space+i" = "S-5"; # %
+      # "space+o" = "S-6"; # ^
+      # "space+p" = "`";
+      #
+      # "space+capslock" = "S-backslash"; # |
+      # "space+a" = "S-9"; # (
+      # "space+s" = "S-["; # {
+      # "space+d" = "[";
+      #
+      # "space+j" = "S-8"; # \
+      # "space+k" = "S-3"; # #
+      # "space+l" = "S-5"; # %
+      # "space+;" = "S-4"; # $
     };
     "extend" = {
       h = "left";
@@ -64,34 +61,34 @@ in {
       i = "pageup";
       o = "end";
 
-      q = "4";
-      w = "5";
-      e = "6";
-      capslock = "0";
-      a = "1";
-      s = "2";
-      d = "3";
-      z = "7";
-      x = "8";
-      c = "9";
+      # q = "4";
+      # w = "5";
+      # e = "6";
+      # capslock = "0";
+      # a = "1";
+      # s = "2";
+      # d = "3";
+      # z = "7";
+      # x = "8";
+      # c = "9";
     };
     "main" = {
-      tab = "f";
-      q = "p";
-      w = "d";
-      e = "l";
-      r = "x";
+      q = "f";
+      w = "p";
+      e = "d";
+      r = "l";
+      t = "x";
       y = ";";
       u = "u";
       i = "o";
       o = "y";
       p = "b";
 
-      capslock = "s";
-      a = "n";
-      s = "t";
-      d = "h";
-      f = "k";
+      a = "s";
+      s = "n";
+      d = "t";
+      f = "h";
+      g = "k";
       h = ",";
       j = "a";
       k = "e";
@@ -99,19 +96,16 @@ in {
       ";" = "c";
       "'" = "z";
 
-      leftshift = "v";
-      z = "w";
-      x = "g";
-      c = "m";
-      v = "j";
-      b = "r";
+      z = "v";
+      x = "w";
+      c = "g";
+      v = "m";
+      b = "j";
       n = "-";
       m = ".";
       "," = "'";
       "." = "=";
-      "backslash" = "backslesh";
-
-      "leftalt" = "r";
+      "backslash" = "backslash";
     };
   };
 }

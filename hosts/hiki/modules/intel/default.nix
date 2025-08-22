f269d5b92a3a60e62e6c1b8ee2e03cfc3f4fd552
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-22 15:36:17 +0900

    test(hiki): update intel graphics config

--- hosts/hiki/modules/intel/default.nix
+++ hosts/hiki/modules/intel/default.nix
@@ -6,5 +6,11 @@
       intel-vaapi-driver
       vpl-gpu-rt
     ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [intel-vaapi-driver];
   };
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
+  environment.systemPackages = with pkgs; [
+    intel-gpu-tools
+    libva-utils
+  ];
 }

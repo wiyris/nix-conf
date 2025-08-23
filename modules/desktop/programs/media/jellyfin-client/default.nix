72eeda19084d7a531efbe1086b95216581035a29
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-24 01:55:25 +0900

    typo

--- modules/desktop/programs/media/jellyfin-client/default.nix
+++ modules/desktop/programs/media/jellyfin-client/default.nix
@@ -6,10 +6,10 @@
 }: let
   cfg = config.custom.programs.jellyfin-client;
 in {
-  options.custom.programs.jellyfin.client.enable = lib.mkEnableOption {};
+  options.custom.programs.jellyfin-client.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm = {
-      home.pacakegs = with pkgs; [
+      home.packages = with pkgs; [
         jellyfin-media-player
         jellyfin-rpc
       ];

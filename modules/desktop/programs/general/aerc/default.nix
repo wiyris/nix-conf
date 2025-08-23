70d2b6a90779f9e4b8bc45495bad6d3a872c3743
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-24 02:31:49 +0900

    fix: fix syntax

--- modules/desktop/programs/general/aerc/default.nix
+++ modules/desktop/programs/general/aerc/default.nix
@@ -4,20 +4,21 @@
   config,
   ...
 }: let
-  cfg = config.nmasur.presets.programs.aerc;
+  cfg = config.custom.programs.aerc;
 in {
-  options.nmasur.presets.programs.aerc.enable = lib.mkEnableOption "Aerc email TUI";
-
+  options.custom.programs.aerc.enable = lib.mkEnableOption "Aerc email TUI";
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      w3m # Render HTML
-      dante # Socksify for rendering HTML
-      aba # Address book
-    ];
+    hm = {
+      home.packages = with pkgs; [
+        w3m # Render HTML
+        dante # Socksify for rendering HTML
+        aba # Address book
+      ];
 
-    programs.aerc = {
-      enable = true;
-      extraBinds = {
+      programs.aerc = {
+        enable = true;
+        extraBinds = {
+        };
       };
     };
   };

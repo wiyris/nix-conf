e9d6811b823baff093f507b933027dce0e460840
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-08-25 00:51:06 +0900

    test

--- hosts/hiki/modules/wireguard/default.nix
+++ hosts/hiki/modules/wireguard/default.nix
@@ -2,7 +2,7 @@
   # enable NAT
   networking.nat.enable = true;
   networking.nat.externalInterface = "enp2s0";
-  networking.nat.internalInterfaces = ["wg0"];
+  networking.nat.internalInterfaces = ["wg42"];
   networking.firewall = {
     allowedUDPPorts = [51820];
   };
@@ -12,7 +12,7 @@
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = ["10.100.0.1/24"];
+      ips = ["10.142.0.42/24"];
 
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = 51820;
@@ -20,12 +20,12 @@
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp2s0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.142.0.42/24 -o enp2s0 -j MASQUERADE
       '';
 
       # This undoes the above command
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp2s00 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.142.0.42/24 -o enp2s00 -j MASQUERADE
       '';
 
       # Path to the private key file.

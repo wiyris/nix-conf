05e911f67af9dd80a95ae4a475d484f042ec171c
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-01 07:47:54 +0900

    chore(sops): update kavita token

--- hosts/hiki/modules/kavita/default.nix
+++ hosts/hiki/modules/kavita/default.nix
@@ -4,13 +4,13 @@ in {
   networking.firewall.allowedTCPPorts = [servicePort];
   services.kavita = {
     enable = true;
-    tokenKeyFile = config.sops.secrets.kavita.path;
+    tokenKeyFile = config.sops.secrets.kavita-token.path;
     settings = {
       Port = servicePort;
     };
   };
 
-  sops.secrets.kavita = {
+  sops.secrets.kavita-token = {
     owner = "kavita";
     group = "kavita";
     sopsFile = ../../../../secrets/secrets.yaml;

a1915c8422fa058fe1cb25fda3f8f678e8ea3ab2
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-01 07:33:15 +0900

    test

--- hosts/hiki/modules/kavita/default.nix
+++ hosts/hiki/modules/kavita/default.nix
@@ -1,10 +1,18 @@
-{
-  networking.firewall.allowedTCPPorts = [5000];
+{config, ...}: let
+  servicePort = 5005;
+in {
+  networking.firewall.allowedTCPPorts = [servicePort];
   services.kavita = {
     enable = true;
-    tokenKeyFile = "/var/lib/kavita/token.key";
+    tokenKeyFile = "config.sops.secrets.kavita.path";
     settings = {
-      Port = 5000;
+      Port = "${servicePort}";
     };
+  };
+
+  sops.secrets.kavita-secret = {
+    owner = "kavita";
+    group = "kavita";
+    sopsFile = ../../../../secrets/secrets.yml;
   };
 }

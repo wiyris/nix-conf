e9581a570406188b6b20afdea4f6f4f654ae41f6
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-08 06:43:24 +0900

    feat(foot): enable server mode

--- modules/desktop/programs/terminal/foot/default.nix
+++ modules/desktop/programs/terminal/foot/default.nix
@@ -14,6 +14,7 @@ in {
     (lib.mkIf cfg.enable {
       hm.programs.foot = {
         enable = true;
+        server.enable = true;
         settings = {
           main = {
             # pad = "4x2";

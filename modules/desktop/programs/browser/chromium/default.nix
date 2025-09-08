cf4f50d5f306932494836d7bc5d03b7f21fb2f67
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-08 12:03:59 +0900

    chore(chromium): disable stylix autorice

--- modules/desktop/programs/browser/chromium/default.nix
+++ modules/desktop/programs/browser/chromium/default.nix
@@ -8,6 +8,7 @@
 in {
   options.custom.programs.chromium.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
+    stylix.targets.chromium.enable = false;
     # TODO: setup ungoogled-chromium extensions w/ home-manager
     # guide: https://gist.github.com/MaximilianGaedig/acbce27522c997e9666bd93cef77492d
     hm.programs.chromium = {

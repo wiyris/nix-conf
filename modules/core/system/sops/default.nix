f4e3d46a8d61e34d3135ac4c89ebfb47db20f331
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-12 08:06:04 +0900

    refactor(sops): update path

--- modules/core/system/sops/default.nix
+++ modules/core/system/sops/default.nix
@@ -1,13 +1,18 @@
 {
+  config,
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  inherit (config.globals) homeDirectory configDirectory;
+in {
   imports = [inputs.sops-nix.nixosModules.sops];
   sops = {
-    defaultSopsFile = ../../../secrets/secrets.yaml;
-    age.keyFile = "/home/tsubaki/.config/sops/age/keys.txt";
+    defaultSopsFile = "${configDirectory}/secrets/secrets.yaml";
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
     age.generateKey = true;
   };
-  hm'.home.packages = [pkgs.sops];
+  environment.systemPackages = [
+    pkgs.sops
+  ];
 }

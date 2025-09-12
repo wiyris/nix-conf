2b5f29c9379a4e0f7a52dba9fe570ee1d99cbddd
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-12 16:01:10 +0900

    chore(dwl): legcord

--- modules/desktop/wm/dwl/config.h
+++ modules/desktop/wm/dwl/config.h
@@ -33,6 +33,7 @@ static const Rule rules[] = {
 	/* examples: */
 	{ "Gimp_EXAMPLE",     NULL,       0,            1,           -1 }, /* Start on currently visible tags floating, not tiled */
 	{ "thunderbird",  NULL,       1 << 8,       0,           -1 }, /* Start on ONLY tag "9" */
+	{ "legcord",  NULL,       1 << 8,       0,           -1 }, /* Start on ONLY tag "9" */
 };
 
 /* layout(s) */

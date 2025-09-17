46f30303927b0fea684fe385c4d7e233dec286dc
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-17 20:07:51 +0900

    chore(zellij): update config

--- modules/core/programs/zellij/default.nix
+++ modules/core/programs/zellij/default.nix
@@ -8,23 +8,14 @@ in {
   options.programs'.zellij.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm' = {
+      imports = [./dots/binds.nix];
       programs.zellij = {
         enable = true;
         # enableFishIntegration = lib.mkIf config.programs.fish.enable true;
-        themes = {
-          stylix = {
-            themes = {
-              default = {
-                frame_selected = {
-                  base = lib.mkForce "#f5c2e7";
-                };
-              };
-            };
-          };
-        };
+        themes.stylix.themes.default.frame_selected.base = lib.mkForce "#f5c2e7";
       };
       home.shellAliases = {
-        ij = "zellij";
+        el = "zellij";
       };
     };
   };

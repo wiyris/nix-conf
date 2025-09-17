001e216bcbead248f0ee7b9cfc94e52576675598
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-17 12:39:48 +0900

    feat(zellji): add alias

--- modules/core/programs/zellij/default.nix
+++ modules/core/programs/zellij/default.nix
@@ -7,9 +7,14 @@
 in {
   options.programs'.zellij.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm'.programs.zellij = {
-      enable = true;
-      enableFishIntegration = lib.mkIf config.programs.fish.enable true;
+    hm' = {
+      programs.zellij = {
+        enable = true;
+        enableFishIntegration = lib.mkIf config.programs.fish.enable true;
+      };
+      home.shellAliases = {
+        ij = "zellij";
+      };
     };
   };
 }

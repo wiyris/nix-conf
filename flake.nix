861de95ff9630ee803377d6d7289ea63308768c7
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-19 14:22:26 +0900

    feat(flake): add stash input

--- flake.nix
+++ flake.nix
@@ -76,6 +76,11 @@
       inputs.home-manager.follows = "home-manager";
     };
 
+    stash = {
+      url = "github:notashelf/stash";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";

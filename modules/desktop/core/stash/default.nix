689952bab4591e5e73fe6df321063e5cd106fa0a
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-19 14:22:03 +0900

    chore: update lockfiles

---
+++ modules/desktop/core/stash/default.nix
@@ -0,0 +1,14 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  stashPkg = inputs.stash.packages.${pkgs.stdenv.hostPlatform}.stash;
+in {
+  environment.systemPackages = [stashPkg];
+
+  # Additionally feel free to add the Stash package in `systemd.packages` to
+  # automatically run the Stash watch daemon, which will watch your primary
+  # clipboard for changes and persist them.
+  systemd.packages = [stashPkg];
+}

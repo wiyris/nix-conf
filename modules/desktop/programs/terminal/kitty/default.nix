c3f6c54212ddc0afd4640099d26ce11459f5a28d
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-09-22 14:54:38 +0900

    feat(kitty):

--- modules/desktop/programs/terminal/kitty/default.nix
+++ modules/desktop/programs/terminal/kitty/default.nix
@@ -21,22 +21,22 @@ in {
           enable_audio_bell = false;
           close_on_child_death = true;
           cursor_blink_interval = 0;
-          background_blur = 1;
+          # background_blur = 1;
           shell_integration = true;
-          confirm_os_window_close = 0;
+          confirm_os_window_close = 2;
 
           enabled_layouts = "fat, tall, vertical";
           wayland_titlebar_color = "background";
 
           allow_remote_control = true;
           listen_on = "unix:/tmp/kitty";
-          dynamic_background_opacity = true;
+          # dynamic_background_opacity = true;
 
           window_padding_width = 10;
           tab_bar_margin_width = 5;
 
-          scrollback_pager = "less --chop-long-lines --raw-control-chars +INPUT_LINE_NUMBER";
-          clipboard_control = "write-clipboard read-clipboard";
+          # scrollback_pager = "less --chop-long-lines --raw-control-chars +INPUT_LINE_NUMBER";
+          # clipboard_control = "write-clipboard read-clipboard";
         };
       };
     })

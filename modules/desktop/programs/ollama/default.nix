c37592029611e87097f93097ec6c0fa4f489b6d1
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-06 13:43:19 +0900

    test: add openwebui

--- modules/desktop/programs/ollama/default.nix
+++ modules/desktop/programs/ollama/default.nix
@@ -7,9 +7,18 @@
 in {
   options.programs'.ollama.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm'.services.ollama = {
+    services.ollama = {
       enable = true;
       acceleration = "rocm";
+      rocmOverrideGfx = "10.3.0";
+    };
+    services.open-webui = {
+      enable = true;
+      openFirewall = true;
+      # environment = {WEBUI_AUTH = "False";};
+    };
+    systemd.services = {
+      ollama.wantedBy = [];
     };
   };
 }

0357db7eeafae61c02a8d8aa56adc0e70be119b9
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-08 16:22:34 +0900

    feat(fonts): enable fontconfig

--- modules/desktop/core/fonts/default.nix
+++ modules/desktop/core/fonts/default.nix
@@ -9,15 +9,21 @@ in {
   options.desktop.fonts.enable = lib.mkEnableOption {};
 
   config = lib.mkIf cfg.enable {
-    fonts.packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-extra
-      noto-fonts-emoji
+    fonts = {
+      packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-extra
+        noto-fonts-emoji
 
-      nerd-fonts.noto
-      nerd-fonts.jetbrains-mono
-    ];
+        nerd-fonts.noto
+        nerd-fonts.jetbrains-mono
+      ];
+      fontconfig = {
+        enable = true;
+        allowBitmaps = true;
+      };
+    };
   };
 }

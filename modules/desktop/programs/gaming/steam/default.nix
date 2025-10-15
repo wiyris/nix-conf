bb54f0b0d19437529ccba023d1df0f155ddeede4
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-15 21:02:03 +0900

    steam: remove scaling option

--- modules/desktop/programs/gaming/steam/default.nix
+++ modules/desktop/programs/gaming/steam/default.nix
@@ -7,25 +7,20 @@
   cfg = config.gaming'.steam;
 in {
   options.gaming'.steam.enable = lib.mkEnableOption {};
-  config = lib.mkMerge [
-    (lib.mkIf config.laptop.enable {
-      environment.sessionVariables.STEAM_FORCE_DESKTOPUI_SCALING = 2;
-    })
-    (lib.mkIf cfg.enable {
-      programs.steam = {
-        enable = true;
-        extraCompatPackages = with pkgs; [proton-ge-bin];
-        protontricks.enable = true;
-        # remotePlay.openFirewall = true;
-        # dedicatedServer.openFirewall = true;
-      };
-      hm'.home.packages = with pkgs; [
-        gamescope
-        steamcmd
-        steam-tui
-        mangohud
-        protonup-rs
-      ];
-    })
-  ];
+  config = lib.mkIf cfg.enable {
+    programs.steam = {
+      enable = true;
+      extraCompatPackages = with pkgs; [proton-ge-bin];
+      protontricks.enable = true;
+      # remotePlay.openFirewall = true;
+      # dedicatedServer.openFirewall = true;
+    };
+    hm'.home.packages = with pkgs; [
+      gamescope
+      steamcmd
+      steam-tui
+      mangohud
+      protonup-rs
+    ];
+  };
 }

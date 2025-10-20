b44baaadb8edd8d1d38d6d79cdf2ae4bb9e68c20
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-20 15:44:35 +0900

    feat(fish): update killall abbrs

--- modules/core/programs/fish/abbrs.nix
+++ modules/core/programs/fish/abbrs.nix
@@ -1,7 +1,8 @@
 {
   programs.fish = {
     shellAbbrs = {
-      kan = "killall -9";
+      k = "killall";
+      kn = "killall -9";
       xx = "7z";
       vol = "wpctl set-volume '@DEFAULT_AUDIO_SINK@'";
       yt = "yt-dlp";

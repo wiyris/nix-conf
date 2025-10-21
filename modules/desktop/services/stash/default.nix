c1f6bc5c4b5ca2401ac9a1b7f56234f818b01497
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-21 16:58:18 +0900

    feat(stash):

--- modules/desktop/services/stash/default.nix
+++ modules/desktop/services/stash/default.nix
@@ -5,13 +5,18 @@
   pkgs,
   ...
 }: let
+  inherit (pkgs) writeText;
   cfg = config.services'.stash;
   stash = inputs.stash.packages.${pkgs.system}.default;
+  regex = "(password|secret|api[_-]?key|token)[=: ]+[^\s]+";
 in {
   options.services'.stash.enable = lib.mkEnableOption [];
   config = lib.mkIf cfg.enable {
     hm' = {
-      home.packages = [stash];
+      home.packages = [
+        stash
+        # pkgs.wl-clipboard-rs
+      ];
       systemd.user.services.stash = {
         Install = {
           WantedBy = ["graphical-session.target"];
@@ -26,6 +31,7 @@ in {
           ExecStart = "${lib.getExe stash} watch";
           Restart = "on-failure";
           # LoadCredential = "clipboard_filter:/etc/stash/clipboard_filter";
+          LoadCredential = "clipboard_filter:${writeText "stash-regex" regex}";
         };
       };
     };

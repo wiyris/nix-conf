1e719de0d1a975bcc44fee8d69ecdf26a14bf291
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-21 12:24:17 +0900

    feat(qutebrowser): rice

--- modules/desktop/programs/browser/qutebrowser/default.nix
+++ modules/desktop/programs/browser/qutebrowser/default.nix
@@ -4,11 +4,38 @@
   ...
 }: let
   cfg = config.programs'.qutebrowser;
+  inherit
+    (config.lib.stylix.colors.withHashtag)
+    base00
+    ;
 in {
   options.programs'.qutebrowser.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm'.programs.qutebrowser = {
       enable = true;
+      settings = {
+        colors = {
+          hints = {
+          };
+          tabs.bar.bg = lib.mkForce "#00000000";
+        };
+        hints = {
+          radius = 1;
+        };
+        scrolling = {
+          bar = "never";
+          smooth = false;
+        };
+        tabs = {
+          show = "multiple";
+          position = "left";
+          width = "15%";
+          background = true;
+          last_close = "close";
+          mode_on_change = "restore";
+          close_mouse_button = "right";
+        };
+      };
     };
   };
 }

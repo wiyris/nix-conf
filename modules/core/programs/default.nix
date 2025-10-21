db8cdcb4ca073ca4cb605fb1193e9127d5c2115e
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-21 17:06:59 +0900

    refactor(core): extract core pkgs

--- modules/core/programs/default.nix
+++ modules/core/programs/default.nix
@@ -1,34 +1,24 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  cfg = config.programs'.core-utils;
-in {
-  options.programs'.core-utils.enable = lib.mkEnableOption {};
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      age
-      curl
-      ffmpeg # record, convert music/video
-      ffmpegthumbnailer
-      file # print filetype
-      git
-      gnupg
-      gtrash
-      imagemagick # image editor
-      jq # manipulate JSON
-      killall
-      lsof # print port process
-      p7zip
-      pinentry
-      unar
-      unzip
-      vim
-      wget
-      wget # download http/ftp
-      yq # manipulate YAML
-    ];
-  };
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    age
+    curl
+    ffmpeg # record, convert music/video
+    ffmpegthumbnailer
+    file # print filetype
+    git
+    gnupg
+    gtrash
+    imagemagick # image editor
+    jq # manipulate JSON
+    killall
+    lsof # print port process
+    p7zip
+    pinentry
+    unar
+    unzip
+    vim
+    wget
+    wget # download http/ftp
+    yq # manipulate YAML
+  ];
 }

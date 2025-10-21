f2f82db432d6123656d63d290e43e663bfdce7b7
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-21 18:18:08 +0900

    feat(core): update aliases

--- modules/core/programs/aliases/default.nix
+++ modules/core/programs/aliases/default.nix
@@ -4,16 +4,24 @@
   ...
 }: let
   cfg = config.programs'.aliases;
+  mycity = config.sops.secrets.mycity;
 in {
   options.programs'.aliases.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
     hm'.home.shellAliases = {
+      c = "clear";
+      p = "cd -";
+      # k = "killall";
+      # kn = "killall -9";
+      pa = "ps aux | grep $1";
+      df = "df -h";
+      du = "du -h -d 1";
+      rm = "rm -Iv";
       mv = "mv -i";
-      cp = "cp -i";
-      pd = "cd -";
       xx = "7z";
-      weather = "curl wttr.in";
+
       myip = "curl ip.me";
+      # weat = "curl wttr.in/${mycity}?2F";
     };
   };
 }

4ac2bc4db74cfbc86652f2978a0d9e9a8ea7e4e5
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-17 14:17:54 +0900

    feat: update core pkgs

--- modules/core/programs/default.nix
+++ modules/core/programs/default.nix
@@ -3,20 +3,19 @@
     age
     curl
     ffmpeg # record, convert music/video
-    ffmpegthumbnailer
+    # ffmpegthumbnailer
     file # print filetype
     git
     gnupg
-    gtrash
+    # gtrash
     imagemagick # image editor
     jq # manipulate JSON
     killall
-    lsof # print port process
+    # lsof # print port process
     p7zip
     unar
     unzip
     vim
-    wget
     wget # download http/ftp
     yq # manipulate YAML
   ];

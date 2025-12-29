233bf716aca10e153afbb13d1e537e0b6375cfac
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-12-30 02:10:07 +0900

    dwl: set librewolf as default browser

--- modules/desktop/wm/dwl/config.h
+++ modules/desktop/wm/dwl/config.h
@@ -61,7 +61,7 @@ static const int repeat_delay = 200;
 static const int tap_to_click = 1;
 static const int tap_and_drag = 1;
 static const int drag_lock = 1;
-static const int natural_scrolling = 0;
+static const int natural_scrolling = 1;
 static const int disable_while_typing = 1;
 static const int left_handed = 0;
 static const int middle_button_emulation = 0;
@@ -85,7 +85,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "uwsm-app", "--", "foot", NULL };
-static const char *browser[] = { "uwsm-app", "--", "zen-beta", NULL };
+static const char *browser[] = { "uwsm-app", "--", "librewolf", NULL };
 static const char *menucmd[] = { "tofi-launcher", "--uwsm", NULL };
 static const char *ustop[] = { "uwsm", "stop", NULL };
 

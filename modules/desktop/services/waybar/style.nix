ebce7f9d77e6bdb74f9e32546653ea1fed696d78
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-01-12 04:30:31 +0900

    style

--- modules/desktop/services/waybar/style.nix
+++ modules/desktop/services/waybar/style.nix
@@ -6,18 +6,21 @@
 {
   stylix.targets.waybar.addCss = false;
   home.packages = [ pkgs.nerd-fonts.noto ];
-  programs.waybar.style = lib.mkAfter ''
-    * { background: @base00; padding:0 7px; }
-    #idle_inhibitor { font-size: 22pt; }
-    #idle_inhibitor.activated { color: @base0C; }
-    #workspaces * { font-size: 12px; padding: 1px; }
-    #workspaces button { color: @base04; }
-    #workspaces button.empty { color: @base02; }
-    /* #workspaces button.active { color: @base07; } */
-    #workspaces button.active { color: #f5c2e7; }
-    #network.disconnected { color: @base0E; }
-    #pulseaudio { min-width: 65px; }
-    #pulseaudio.muted { color: @base08; }
-    #tray * { padding: 0; }
-  '';
+  programs.waybar.style =
+    lib.mkAfter
+      # css
+      ''
+        * { background: @base00; padding:0 7px; }
+        #idle_inhibitor { font-size: 22pt; }
+        #idle_inhibitor.activated { color: @base0C; }
+        #workspaces * { font-size: 12px; padding: 1px; }
+        #workspaces button { color: @base04; }
+        #workspaces button.empty { color: @base02; }
+        /* #workspaces button.active { color: @base07; } */
+        #workspaces button.active { color: #f5c2e7; }
+        #network.disconnected { color: @base0E; }
+        #pulseaudio { min-width: 65px; }
+        #pulseaudio.muted { color: @base08; }
+        #tray * { padding: 0; }
+      '';
 }

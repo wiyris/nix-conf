ba9b3ad1247b28e5ff835774ca33424801d11b7b
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-02-09 18:18:24 +0900

    refactor(gamemope):

--- modules/desktop/programs/gaming/gamemode/default.nix
+++ modules/desktop/programs/gaming/gamemode/default.nix
@@ -23,8 +23,8 @@ in
           amd_performance_level = "high";
         };
         custom = {
-          start = "${pkgs.libnotify}/bin/notify-send -u low 'GameMode started'";
-          end = "${pkgs.libnotify}/bin/notify-send -u low 'GameMode ended'";
+          start = "${lib.getExe pkgs.libnotify} -u low 'GameMode started'";
+          end = "${lib.getExe pkgs.libnotify} -u low 'GameMode ended'";
         };
       };
     };
